d645f63ffe678bbcdee3978438adbb18afa84b82
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-08-23 12:26:52 +0300

    Section 3 - Adding the repo methods

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,34 +10,28 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
         // Lifetime details of this dependency injection: 
         // When request is made at this route, the framework instantiates a new ApiController. 
-        // To do so, a new DBContext will be instantiated as a service.
+        // To do so, a new repo and DBContext will be instantiated as a service.
         // When the request is finished, both will be disposed of.
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
 
-        [HttpGet]
+        [HttpGet] //Returned type for HTTP requests should be ActionResult<> 
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            // Async programming:
-            // The thread creates a Task to handle the DB communication.
-            // While the Task executes in the background (probably by some other thread),
-            // this thread is free to handle another HTTP request.
-            // When the Task finished, it notifies this thread to collect the data.
-            var products = await _context.Products.ToListAsync();
-
-            return products; // no need for 2 separate lines
+            var products = await _repo.GetProductsAsync();
+            return Ok(products); // no need for 2 separate lines
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
     }
 }
