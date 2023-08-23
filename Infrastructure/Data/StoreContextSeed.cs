313e28ca5d11eeafa4ccbef0bec3cf9d93c1bdce
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-08-23 13:29:51 +0300

    S3) Adding seed data

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,41 @@
+using System.Text.Json;
+using Core.Entities;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context)
+        {
+            if (!context.ProductBrands.Any())
+            {
+                // Read JSON files, deserialize JSON into C# objects and add them to DbContext.
+                // We do not use the async versions of the next 3 statements, because we just track what is happening in memory, without calling the DB
+
+                // This method will be called by the startup project (API). Thus we need to navigate to Infrastructure project first.
+                var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                context.ProductBrands.AddRange(brands);
+            }
+
+            if (!context.ProductTypes.Any())
+            {
+                var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+                var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+                context.ProductTypes.AddRange(types);
+            }
+
+            if (!context.ProductTypes.Any()) // Seed this after ProductBrands and ProductTypes, since it holds references to those tables
+            {
+                var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+                context.Products.AddRange(products);
+            }
+
+            if (context.ChangeTracker.HasChanges())
+            {
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
