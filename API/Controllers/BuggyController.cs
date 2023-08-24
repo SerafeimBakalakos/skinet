1b03fcc8578b9188f8165569564b492e30b15696
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-08-24 21:09:19 +0300

    S5) Consistent error response from the API

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,3 +1,4 @@
+using API.Errors;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,7 +20,7 @@ namespace API.Controllers
             var thing = _context.Products.Find(42);
             if (thing == null)
             {
-                return NotFound();
+                return NotFound(new ApiResponse(404));
             }
             return Ok();
         }
@@ -36,7 +37,7 @@ namespace API.Controllers
         [HttpGet("badrequest")]
         public ActionResult GetBadRequest()
         {
-            return BadRequest(); // 400-ish response
+            return BadRequest(new ApiResponse(400)); // 400-ish response
         }
 
         [HttpGet("badrequest/{id}")]
