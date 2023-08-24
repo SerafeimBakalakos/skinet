80c18a474cddc65a5f5331b13db0e13b36508698
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-08-24 16:36:28 +0300

    S4) Getting single object with specification

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -6,9 +6,19 @@ using Core.Entities;
 
 namespace Core.Specifications
 {
+    /// <summary>
+    /// Used for eager loading of navigation properties when requesting one or more Products
+    /// </summary>
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
         public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(p => p.ProductType);
+            AddInclude(p => p.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int productId) 
+        : base(p => p.Id == productId)
         {
             AddInclude(p => p.ProductType);
             AddInclude(p => p.ProductBrand);
