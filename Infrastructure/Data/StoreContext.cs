fdf0fb4a64eef418ccff1f1a08d4a83cde5aa980
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-08-25 15:06:00 +0300

    S6) Decimals in SQLite are converted to double

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -17,7 +17,26 @@ namespace Infrastructure.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            // Tell the ModelBuilder for this DbContext, that there are configurations in files from the Assembly
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // With SQLite we need to convert decimal to double for all entities that use decimal properties
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite") // too easy to make a typo here, which will not throw an error, but will prevent this code from working
+            {
+                // Access each entity classes (e.g. Product) in the DB
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    // Find decimal properties of this entity class
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach (var property in properties)
+                    {
+                        // Tell the ModelBuilder to convert this decimal property to double
+                        modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
+                    }
+                }
+            }
         }
     }
 }
