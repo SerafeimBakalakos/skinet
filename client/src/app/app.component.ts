3bbb6db570a3480959e60d69d487749ba4f79b21
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-08-28 20:40:23 +0300

    S8) Create a products interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
 // OnInit is a lifecycle hook
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: any[] = []; // initial value is necessary when using strict mode in TS
+  products: IProduct[] = []; // initial value is necessary when using strict mode in TS
 
   // Services can injected into the ctor of AppComponent
   // However it is too early to make an API request here
@@ -19,8 +21,8 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     // Best practice: make request to the API here.
     // GET returns an observable and we need to subscribe to it (otherwise the request will be skipped) using an observer object {next:..., error:..., complete:...}
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: (response: any) => this.products = response.data, // what to do after the response is received
+    this.http.get<IPagination<IProduct[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: response => this.products = response.data, // what to do after the response is received
       error: error => console.log(error), // what to do in case of error
       complete: () => {
         console.log('request completed');
