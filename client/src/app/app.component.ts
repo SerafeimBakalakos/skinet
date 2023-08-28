b591cc4ce1086708a5774d2d392520ff1bcfd242
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-08-28 17:57:11 +0300

    S8) Intro to HttpClientModule

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+// OnInit is a lifecycle hook
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: any[] = []; // initial value is necessary when using strict mode in TS
+
+  // Services can injected into the ctor of AppComponent
+  // However it is too early to make an API request here
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit(): void {
+    // Best practice: make request to the API here.
+    // GET returns an observable and we need to subscribe to it (otherwise the request will be skipped) using an observer object {next:..., error:..., complete:...}
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: (response: any) => this.products = response.data, // what to do after the response is received
+      error: error => console.log(error), // what to do in case of error
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statements');
+      }
+    }) // after "complete", we automatically unsubscribe
+  }
 }
