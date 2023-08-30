0f68f90aeee2cd6a5cb76d92048a5289f1d9c0bf
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-08-30 18:04:40 +0300

    S9) Intro to Angular services

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IPagination } from '../shared/models/pagination';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseUrl = 'https://cocalhost:5001/api/';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getProducts() {
+    return this.http.get<IPagination<IProduct[]>>(this.baseUrl + 'products');
+  }
 }
