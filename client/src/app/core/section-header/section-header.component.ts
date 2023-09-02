97c93690e2e840798064e13453912de821161729
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-09-02 10:19:10 +0300

    S12) Setting the section header title from the breadcrumb service

--- client/src/app/core/section-header/section-header.component.ts
+++ client/src/app/core/section-header/section-header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-section-header',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./section-header.component.scss']
 })
 export class SectionHeaderComponent {
+
+  //public allows us to use "bcService" inside the html file (section-header.component.html)
+  constructor(public bcService:BreadcrumbService) {
+    bcService.breadcrumbs$ //$ suffix is naming convention for observable
+    //
+  }
 
 }
