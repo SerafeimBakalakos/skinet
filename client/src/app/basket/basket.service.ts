80e26f65018c1935e71d24f2be1c192f0054b31e
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-09-04 16:25:35 +0300

    S14) Adding an item to the basket service method

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { IBasket } from '../shared/models/basket';
+import { Basket, IBasket, IBasketItem } from '../shared/models/basket';
 import { HttpClient } from '@angular/common/http';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -38,5 +39,43 @@ export class BasketService {
   // In "getBasket" we update "basketSource" from the API and then notify the components. Here we just view and return a property of the client-side "basketSource". Naming both "get..." is very confusing.
   getCurrentBasketValue() {
     return this.basketSource.value;
+  }
+
+  addItemToBasket(item:IProduct, quantity=1) {
+    const itemToAdd = this.mapProductItemToBasketItem(item);
+    // "getCurrentBasketValue()"" can return null, if there is no basket stored in the API
+    const basket = this.getCurrentBasketValue() ?? this.createBasket();
+    basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
+  }
+
+  private addOrUpdateItem(items: IBasketItem[], itemToAdd: IBasketItem, quantity: number): IBasketItem[] {
+    const item = items.find(x => x.id === itemToAdd.id);
+    if (item) item.quantity += quantity;
+    else {
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    }
+    return items;
+  }
+
+  private createBasket(): IBasket {
+    const basket = new Basket();
+
+    // persist the id in browser's local storage. It will persist even after computer reboots
+    localStorage.setItem('basket_id', basket.id); 
+    return basket;
+  }
+
+  private mapProductItemToBasketItem(product:IProduct): IBasketItem {
+    return {
+      id: product.id,
+      productName: product.name,
+      price: product.price,
+      quantity: 0,
+      prictureUrl: product.pictureUrl,
+      brand: product.productBrand,
+      productType: product.productType
+    }
   }
 }
