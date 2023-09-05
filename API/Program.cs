addcfb91f768d6072bea6962bf7e122607c2d292
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-09-05 20:42:41 +0300

    S15) Adding the identity to Program

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,11 @@
 using API.Errors;
 using API.Extensions;
 using API.Middleware;
+using Core.Entities.Identity;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -37,6 +40,8 @@ app.UseStaticFiles(); //position before UseAuthorization
 
 //app.UseHttpsRedirection();
 app.UseCors("CorsPolicy"); // Must be just before UseAuthorization(). Use the same name as when defining the policy in the services.
+
+app.UseAuthentication(); // Always authentication before authorization
 app.UseAuthorization();
 
 app.MapControllers();
@@ -45,11 +50,15 @@ app.MapControllers();
 using var scope = app.Services.CreateScope(); // Get a temporary Scope to access services
 var services = scope.ServiceProvider;
 var context = services.GetRequiredService<StoreContext>();
+var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+var userManager = services.GetRequiredService<UserManager<AppUser>>();
 var logger = services.GetRequiredService<ILogger<Program>>();
 try
 {
     await context.Database.MigrateAsync();
+    await identityContext.Database.MigrateAsync();
     await StoreContextSeed.SeedAsync(context);
+    await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
 }
 catch (Exception ex)
 {
