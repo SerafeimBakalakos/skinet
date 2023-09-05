5b3fda4541de60b2966a0880c05f056f87f6f946
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-09-05 21:45:25 +0300

    S15) Registering a user

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -40,5 +40,28 @@ namespace API.Controllers
                 DisplayName = user.DisplayName
             };
         }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(new ApiResponse(400));
+            }
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = "This will be a token"
+            };
+        }
     }
 }
