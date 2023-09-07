e51a85cd4e7e36652487fd020f4b856390ad5b48
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-09-07 10:42:02 +0300

    S15) Adding UserManager extensions for retrieving users from HttpContext

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
+using System.Security.Claims;
 using API.Dtos;
 using API.Errors;
+using API.Extensions;
 using Core.Entities.Identity;
 using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,6 +21,52 @@ namespace API.Controllers
             _tokenService = tokenService;
             _signInManager = signInManager;
             _userManager = userManager;
+        }
+
+        /// <summary>
+        /// The client sends an HTTP GET request to "account". That HTTP GET also contains a token. 
+        /// This method then returns the user that corresponds to that token. 
+        [Authorize]
+        [HttpGet] // no route: put it at the top of the other methods
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            // After authentication, HttpContext contains data about the user.
+
+            // Option 1: Get email from HttpContext and then user from UserManager
+            // string email = HttpContext.User?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.Email).Value;
+            // AppUser user = await _userManager.FindByEmailAsync(email);
+
+            // Option 2: Get email from the ClaimsPrincipal named User and then user from UserManager
+            // string email = User.FindFirstValue(ClaimTypes.Email);
+            // AppUser user = await _userManager.FindByEmailAsync(email);
+            
+            // Option 3: Same as option 2, but hidden in our own extension methods
+            AppUser user = await _userManager.FindByEmailFromClaimsPrincipal(User);
+            
+            return new UserDto 
+            {
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user),
+                DisplayName = user.DisplayName
+            };
+        }
+        
+        /// <summary>
+        /// Used by the client to asynchronously validate that the email they entered exists.
+        /// </summary>
+        [HttpGet("emailexists")]
+        public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
+        {
+            return await _userManager.FindByEmailAsync(email) != null;
+        }
+
+        [Authorize]
+        [HttpGet("address")]
+        public async Task<ActionResult<Address>> GetUserAdress()
+        {
+            // We need to eagerly load the nav properties of AppUser
+            AppUser user = await _userManager.FindUserByClaimsPrincipalWithAddress(User);
+            return user.Address;
         }
 
         [HttpPost("login")]
